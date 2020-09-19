56a12a12882c4618b80635cbc3f4b512fba12655
Iavor Diatchki <iavor.diatchki@gmail.com>
2020-09-18 23:02:57 -0700

    Buttons for WP spaces

--- byzantium/src/spawn.lua
+++ byzantium/src/spawn.lua
@@ -36,9 +36,131 @@ function spawnBoard(k)
     image = board_url
   })
 
-  sem.up(); spawnBlocker(-15.7,-12.5,18,10,sem.down)
+  sem.up(); spawnBlocker(-15.7,-12.5,18,10.5,sem.down)
   sem.up(); spawnBlocker(24,4.3,15,23.5,sem.down)
+
+  local x0 = -1.1
+  local x = x0
+  local y = 0.015
+  local dx = 0.131
+  local dy = 0.131
+  local sz = 60
+
+  local msg = table.concat(
+    { "Bulgar Army (Byzantium/Arabs)"
+    , ""
+    , "1. Increase Bulgar army strength by 2"
+    , "2. Either:"
+    , "     - Attack neighbouring city, or"
+    , "     - Increase strength by 2 more."
+    , ""
+    , "* Strength may not exceed 11."
+    , "* May only use roads."
+    },"\n")
+  spawnTip(x,y,sz,"bulgar-1",msg); x = x + dx
+  spawnTip(x,y,sz,"bulgar-2",msg); x = x + dx
+
+  msg = table.concat(
+    { "Fortify City (Byzantium/Arabs)"
+    , ""
+    , "Add one of your fortifications"
+    , "to a city you control."
+    , ""
+    , "* Fortifications add +1 when"
+    , "  a city is besieged."
+    , ""
+    , "* Fortifications stay with the city"
+    , "  until it is sacked."
+    , ""
+    , "* A city may have only one"
+    , "  foritification."
+    }, "\n")
+  spawnTip(x,y,sz,"fortify-1",msg); x = x + dx
+  spawnTip(x,y,sz,"fortify-2",msg); x = x + dx
+
+  x = x0; y = y + dy
+
+  msg = table.concat(
+    { "Improve City (Byzantium)"
+    , ""
+    , "Add +1 strength to a Byzantine city."
+    , ""
+    , "* No need to control the city."
+    , ""
+    , "* City strength may not exceed 3"
+    , "  (fortifications do not count)."
+    }, "\n")
+  spawnTip(x,y,sz,"byz-improve-1",msg); x = x + dx
+  spawnTip(x,y,sz,"byz-improve-2",msg); x = x + dx
+
+  msg = table.concat(
+    { "Emperor (Byzantium)"
+    }, "\n")
+  spawnTip(x,y,sz,"emperor",msg); x = x + dx
+
+  msg = table.concat(
+    { "Civil War (Byzantium)"
+    }, "\n")
+  spawnTip(x,y,sz,"byz-civil-war",msg); x = x + dx
+
+  msg = table.concat(
+    { "Fleet (Byzantium)"
+    }, "\n")
+  spawnTip(x,y,sz,"byz-fleet",msg); x = x + dx
+
+  x = x0; y = y + dy
+
+  msg = table.concat(
+    { "Improve City (Arab)"
+    , ""
+    , "Add +1 strength to an Arab city."
+    , ""
+    , "* No need to control the city."
+    , ""
+    , "* City strength may not exceed 3"
+    , "  (fortifications do not count)."
+    }, "\n")
+  spawnTip(x,y,sz,"arab-improve-1",msg); x = x + dx
+  spawnTip(x,y,sz,"arab-improve-2",msg); x = x + dx
+  spawnTip(x,y,sz,"arab-improve-3",msg); x = x + dx
+
+  msg = table.concat(
+    { "Caliph (Arab)"
+    }, "\n")
+  spawnTip(x,y,sz,"caliph",msg); x = x + dx
+
+  msg = table.concat(
+    { "Civil War (Arab)"
+    }, "\n")
+  spawnTip(x,y,sz,"arab-civil-war-1",msg); x = x + dx
+  spawnTip(x,y,sz,"arab-civil-war-2",msg); x = x + dx
+
+  msg = table.concat(
+    { "Fleet (Arab)"
+    }, "\n")
+  spawnTip(x,y,sz,"arab-fleet",msg); x = x + dx
+
   sem.wait(k)
+end
+
+
+function spawnTip(x,y,w,id,tip,k)
+  local fg = Color(1,1,1,0)
+  local bg = Color(0,0,0,0)
+  GUI.board.createButton(
+      { font_size      = 0
+      , font_color     = fg
+      , hover_color    = bg
+      , press_color    = bg
+      , color          = bg
+      , label          = id
+      , click_function = "nop"
+      , position       = { x, 5, y }
+      , rotation       = { 0, 0, 0 }
+      , width          = w
+      , height         = w
+      , tooltip        = tip
+      })
 end
 
 function spawnBlocker(x,y,w,h,k)
@@ -78,11 +200,25 @@ function spawnPlayer(pstate,k)
   local dx = ({0,1,1,0})[o]
   local dy = ({0,0,1,1})[o]
   local x = -23.5 + dx * 10
-  local y = -9 + dy *-5
+  local y = -8 + dy *-5
 
   local h = 1
   local w = 1
   local sem = newSem()
+  local function label(tip,msg)
+    sem.up()
+    spawnBox(x,y,Color(0.5,0.5,0.5),Color(0,0,0),tip,msg,sem.down)
+  end
+  local x0 = x
+  label("Elite Army","E"); x = x + w
+  label("Main Arymy","A"); x = x + w
+  label("Levy","L"); x = x + w
+  label("Movement","M"); x = x + 1.5 * w
+  label("Treasury","$"); x = x + 1.5 * w
+  label("VP","VP"); x = x + 1.5 * w
+
+  x = x0
+  y = y - h
   sem.up(); spawnFaction(x,y,byzantium, pstate.factions[byzantium],sem.down)
   y = y - h
   sem.up(); spawnFaction(x,y,arabs, pstate.factions[arabs],sem.down)
@@ -97,6 +233,12 @@ function spawnPlayer(pstate,k)
   info("Available",pstate.available); x = x + w
   info("Casualty",pstate.casualty);   x = x + 2 + 1.5*w
   info("Fortifications",pstate.fortifications)
+
+  y = y - h
+  x = x0
+  label("Available","[00FF00]✓[-]"); x = x + w
+  label("Casualty", "[FF0000]✗[-]"); x = x + 3.5 * w
+  label("Fortifications", "F")
 
   sem.wait(k)
 end
@@ -117,7 +259,7 @@ function spawnFaction(x,y,name,f,k)
   info("Main Army ($1)",f.mainArmy); x = x + w
   info("Levy ($2)",f.levy); x = x + w
   info("Movement ($1)",f.movement); x = x + 1.5 * w
-  info("Treasury", f.treasury); x = x + 1
+  info("Treasury", f.treasury); x = x + 1.5 * w
   info("VP", f.vp)
   sem.wait(k)
 end
@@ -147,6 +289,9 @@ end
 
 
 
+
+
+--------------------------------------------------------------------------------
 function spawnCities(g,k)
   GUI.cities = {}
   local sem = newSem()
@@ -157,7 +302,6 @@ function spawnCities(g,k)
   sem.wait(k)
 end
 
--- XXX: Constantinople square
 function spawnCity(name,city,k)
   local loc = { city.x, 0.2, city.y }
   local s
@@ -252,6 +396,7 @@ function redrawCity(g,name,k)
   o.destroy()
   spawnCity(name,g.map.cities[name],k)
 end
+--------------------------------------------------------------------------------
 
 
 
