4b88b72e60f6b43d15a8a6decfb78b352d92eecb
Iavor Diatchki <iavor.diatchki@gmail.com>
2021-01-16 09:18:13 -0800

    Derive JSON for `BonusToken`

--- ht-hs/src/Bonus.hs
+++ ht-hs/src/Bonus.hs
@@ -2,6 +2,7 @@ module Bonus where
 
 import Data.Text(Text)
 import qualified Data.Aeson as JS
+import GHC.Generics
 
 import Common.Utils(enumAll)
 
@@ -10,17 +11,17 @@ data BonusToken =
   | BonusSwap
   | BonusMove
   | BonusExtra
-  | BonusAct4
   | BonusAct3
-  deriving (Eq,Ord,Show,Read,Enum,Bounded)
+  | BonusAct4
+  deriving (Eq,Ord,Show,Generic,Enum,Bounded)
 
 -- | Not counting the starting ones
 tokenNumber :: BonusToken -> Int
 tokenNumber token =
   case token of
     BonusUpgrade -> 2
-    BonusAct4    -> 2
     BonusAct3    -> 2
+    BonusAct4    -> 2
     BonusSwap    -> 2
     BonusMove    -> 1
     BonusExtra   -> 3
@@ -38,7 +39,7 @@ data FixedBonus =
   | BonusGainPrivilege
   | BonusBuildInGreen
   | BonusReuse2
-  deriving (Eq,Ord,Show,Read)
+  deriving (Eq,Ord,Show)
 
 
 
@@ -46,21 +47,16 @@ data FixedBonus =
 bonusAsKey :: BonusToken -> Text
 bonusAsKey token =
   case token of
-    BonusUpgrade -> "upgrade"
-    BonusSwap    -> "swap"
-    BonusMove    -> "move"
-    BonusExtra   -> "extra"
-    BonusAct4    -> "act_4"
-    BonusAct3    -> "act_3"
+    BonusUpgrade -> "BonusUpgrade"
+    BonusSwap    -> "BonusSwap"
+    BonusMove    -> "BonusMove"
+    BonusExtra   -> "BonusExtra"
+    BonusAct3    -> "BonusAct3"
+    BonusAct4    -> "BonusAct4"
 
-instance JS.ToJSON BonusToken where
-  toJSON = JS.toJSON . bonusAsKey
-
-instance JS.FromJSON BonusToken where
-  parseJSON = JS.withText "bonus token" \t ->
-    case lookup t [ (bonusAsKey b,b) | b <- enumAll ] of
-      Just a    -> pure a
-      Nothing   -> fail "Invalid bonus token"
+instance JS.ToJSON    BonusToken
+instance JS.ToJSONKey BonusToken
+instance JS.FromJSON  BonusToken
 
 
 
